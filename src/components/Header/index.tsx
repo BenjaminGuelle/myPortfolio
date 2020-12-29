109915c818a9e56ab689be5721b32b0ec3574390
Benjamin Guelle <benjamin.guelle@gmail.com>
2020-12-29 18:27:19 +0100

    update nav down && custon burger menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,22 +8,35 @@ const Header = ({ siteTitle }) => {
         <header>
             <nav className="nav">
                 <div className="nav-logo">
-                    LOGO
+                    <span>B.G</span>
                 </div>
-                <div className="ctn-links">
+                <div className="ctn-links_L">
                     <Link to="#" className="nav-link">Accueil</Link>
                     <Link to="#" className="nav-link">Bio</Link>
                     <Link to="#" className="nav-link">Compétences</Link>
                     <Link to="#" className="nav-link">Portfolio</Link>
                     <Link to="#" className="nav-link">Contact</Link>
                 </div>
-                <button type="button" className="btn-mob btn-menu">
-                    Menu
-                </button>
-                <button type="button" className="btn-mob btn-contact">
-                    Contact
-                </button>
+                
+                <button
+                    type="button"
+                    className="btn-menu"
+                    onClick={
+                        () => {
+                            document.querySelector('.ctn-links').classList.toggle('opened');
+                            document.querySelector('.btn-menu span').classList.toggle('active');
+                        }
+                    }>
+                    <span></span>
+                </button>                
             </nav>
+            <div className="ctn-links">
+                <Link to="#" className="nav-link">Accueil</Link>
+                <Link to="#" className="nav-link">Bio</Link>
+                <Link to="#" className="nav-link">Compétences</Link>
+                <Link to="#" className="nav-link">Portfolio</Link>
+                <Link to="#" className="nav-link">Contact</Link>
+            </div>
         </header>
     )
 }
